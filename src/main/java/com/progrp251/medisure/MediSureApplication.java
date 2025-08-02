90e1d2d93fccf42b4aec421da3a25a59fe143e32
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-08-02 18:51:00 +0530

    Initial commit

---
+++ src/main/java/com/progrp251/medisure/MediSureApplication.java
@@ -0,0 +1,13 @@
+package com.progrp251.medisure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediSureApplication.class, args);
+    }
+
+}
