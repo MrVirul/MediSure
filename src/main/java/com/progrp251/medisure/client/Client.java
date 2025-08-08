d3c20baa23766843041a6ea81a0d0f74c906c3c9
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-08-09 00:01:39 +0530

    Add Client entity: define fields with validation for NIC, name, phone number, address, date of birth, email, gender, and blood group

--- src/main/java/com/progrp251/medisure/client/Client.java
+++ src/main/java/com/progrp251/medisure/client/Client.java
@@ -1,4 +1,38 @@
 package com.progrp251.medisure.client;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @Pattern(regexp = "^([0-9]{9}[vVxX]|[0-9]{12})$", message = "Invalid NIC format")
+    private String nic;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Pattern(regexp = "^0[1-9][0-9]{8}$", message = "Invalid Sri Lankan phone number")
+    private String phoneNumber;
+
+    @NotBlank
+    private String address;
+
+    @Past
+    private LocalDate dateOfBirth;
+
+    @Email
+    private String email;
+
+    private String gender;
+    private String bloodGroup;
 }
