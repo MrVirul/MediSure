812901a40f386e5b72dbb3ed1e2571e729dae543
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-08-27 12:53:03 +0530

    Add ClientController with home and login page mappings

--- src/main/java/com/progrp251/medisure/client/ClientController.java
+++ src/main/java/com/progrp251/medisure/client/ClientController.java
@@ -1,11 +1,20 @@
 package com.progrp251.medisure.client;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-
+@Controller
 public class ClientController {
+    @GetMapping({"/", "/index"})
+    public String home() {
+        // Return the Thymeleaf template located at templates/Home/index.html
+        return "Home/index";
+    }
 
-
-
+    @GetMapping("/login")
+    public String loginPage() {
+        // Return the Thymeleaf template located at templates/Home/login.html
+        return "Home/login";
+    }
 }
