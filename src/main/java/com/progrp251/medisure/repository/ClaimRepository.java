10d73b7a8e99bcc6e1114cd8265a5f2a4067504d
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-09-16 19:39:19 +0530

    Refactor Claim and Policy repositories to use new package structure for domain entities

--- src/main/java/com/progrp251/medisure/repository/ClaimRepository.java
+++ src/main/java/com/progrp251/medisure/repository/ClaimRepository.java
@@ -1,7 +1,6 @@
 package com.progrp251.medisure.repository;
 
-import com.progrp251.medisure.domain.entity.Claim;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClaimRepository extends JpaRepository<Claim, Long> {
+public interface ClaimRepository extends JpaRepository<com.progrp251.medisure.claim.Claim, Long> {
 }
--- src/main/java/com/progrp251/medisure/repository/PolicyRepository.java
+++ src/main/java/com/progrp251/medisure/repository/PolicyRepository.java
@@ -1,7 +1,6 @@
 package com.progrp251.medisure.repository;
 
-import com.progrp251.medisure.domain.entity.Policy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
+public interface PolicyRepository extends JpaRepository<com.progrp251.medisure.policy.Policy, Long> {
 }
